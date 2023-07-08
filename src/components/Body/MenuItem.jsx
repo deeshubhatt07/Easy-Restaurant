31d29561652824cd9d33055ce8001a3a1c300bc8
Deepanshu Bhatt <48888110+deeshubhatt07@users.noreply.github.com>
2023-07-08 15:09:57 +0530

    added css body

--- src/components/Body/MenuItem.jsx
+++ src/components/Body/MenuItem.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import {Card,Button} from 'react-bootstrap'
 
 const MenuItem = props => {
-    console.log(props);
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '25rem' }}>
                 <Card.Img variant="top" src={props.listItem.image} />
                 <Card.Body>
-                    <Card.Title>props.listItem.name</Card.Title>
+                    <Card.Title style={{cursor:"pointer"}}>{props.listItem.name}</Card.Title>
                     <Card.Text>
                         {props.listItem.description}
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <p>price - {props.listItem.price}$</p>
                 </Card.Body>
             </Card>
         </div>
