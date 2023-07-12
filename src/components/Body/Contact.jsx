b26a70e8aecb2bc38566e77ee787c1fb61eec434
Deepanshu Bhatt <48888110+deeshubhatt07@users.noreply.github.com>
2023-07-12 12:14:30 +0530

    Added Footer and CSS styling

--- src/components/Body/Contact.jsx
+++ src/components/Body/Contact.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
 
 const Contact = () => {
-  return (
-    <div>
-      <h1>Contact US</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Fragment>
+                <h1>Contact US</h1>
+                <Container>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email Id</Form.Label>
+                            <Form.Control type="email" placeholder="Enter you Email" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPhone">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="text" placeholder="Enter your Phone Number" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Send us your Query</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Container>
+            </Fragment>
+        </div>
+    )
 }
 
 export default Contact
